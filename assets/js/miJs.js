8ae92fcc70ad197b8dd1a272e46f65d263085873
RGonzalezL <rgonzalez.quim@gmail.com>
2022-04-04 10:00:08 -0400

    Primer commit PS5

---
+++ assets/js/miJs.js
@@ -0,0 +1,28 @@
+$(function() {
+
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1800, function() {
+
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+
+    //globo tooltip
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+});
